d45954f89c75723c192419fecc234da6fbc0b25b
Dr. James P Burke <james.burke@sbregional.org>
2025-06-05 13:54:08 -0400

    Add shooting and enemy spawns

--- main.ts
+++ main.ts
@@ -1,58 +1,50 @@
+function setBaddieVel (mySprite: Sprite) {
+    mySprite.setVelocity(randint(-30, 30), randint(-30, 30))
+    bdx = player_1.tilemapLocation().x - mySprite.tilemapLocation().x
+    bdy = player_1.tilemapLocation().y - mySprite.tilemapLocation().y
+    mySprite.setVelocity((bdx + randint(10, bdx / 3)) * (randint(1, 4) / 4), (bdy + randint(10, bdy / 3)) * (randint(1, 4) / 4))
+}
 function midPoint (mySprite: Sprite, mySprite2: Sprite) {
     mid_x = (mySprite.x + mySprite2.x) / 2
     mid_y = (mySprite.y + mySprite2.y) / 2
 }
-function sightCheck (loc0: tiles.Location, loc1: tiles.Location) {
-    dx = Math.abs(loc1.column - loc0.column)
-    dy = Math.abs(loc1.row - loc0.row)
-    let sx = loc0.x < loc1.x ? 1 : -1;
-let sy = loc0.y < loc1.y ? 1 : -1;
-x = loc0.column
-    y = loc0.row
-    steps = Math.max(dx, dy)
-    let err = dx > dy ? dx / 2 : dy / 2;
-allClear = true
-    for (let i = 0; i <= steps; i++) {
-        if (tilecheck(tiles.getTileLocation(x, y))) {
-            allClear = false
-        }
-        if (dx > dy) {
-            err += 0 - dy
-            if (err < 0) {
-                y += sy
-                err += dx
-            }
-            x += sx
-        } else {
-            err += 0 - dx
-            if (err < 0) {
-                x += sx
-                err += dy
-            }
-            y += sy
-        }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (controller.dx() + controller.dy() != 0) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . 4 4 . . . . . . . 
+            . . . . . . 4 5 5 4 . . . . . . 
+            . . . . . . 2 5 5 2 . . . . . . 
+            . . . . . . . 2 2 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, player_2, controller.dx(10000), controller.dy(10000))
     }
-    return allClear
+})
+function monkeyMove2 () {
+	
 }
-function tilecheck (aloc: tiles.Location) {
-    if (tiles.tileAtLocationIsWall(aloc)) {
-        return false
-    } else {
-        return true
-    }
-    return true
-}
+scene.onHitWall(SpriteKind.Enemy, function (sprite, location) {
+    setBaddieVel(sprite)
+})
 function monkeyMove () {
-    if (!(sightCheck(player_1.tilemapLocation(), player_2.tilemapLocation()))) {
-        dx = player_2.tilemapLocation().x - player_1.tilemapLocation().x
-        dy = player_2.tilemapLocation().y - player_1.tilemapLocation().y
-        player_1.setVelocity((dx + randint(10, dx / 3)) * (randint(1, 4) / 4), (dy + randint(10, dy / 3)) * (randint(1, 4) / 4))
-    } else {
-        player_1.setVelocity(randint(-50, 50), randint(-50, 50))
-    }
+    dx = player_2.tilemapLocation().x - player_1.tilemapLocation().x
+    dy = player_2.tilemapLocation().y - player_1.tilemapLocation().y
+    player_1.setVelocity((dx + randint(10, dx / 3)) * (randint(1, 4) / 4), (dy + randint(10, dy / 3)) * (randint(1, 4) / 4))
 }
-let allClear = false
-let steps = 0
+let a_baddie: Sprite = null
+let projectile: Sprite = null
+let bdy = 0
+let bdx = 0
 let player_2: Sprite = null
 let player_1: Sprite = null
 let mid_y = 0
@@ -100,14 +92,47 @@ player_2 = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-player_2.setStayInScreen(true)
-player_1.setStayInScreen(true)
 player_1.setPosition(330, 20)
 player_2.setPosition(360, 10)
 controller.moveSprite(player_2)
+pause(100)
 game.onUpdateInterval(50, function () {
     midPoint(player_1, player_2)
     scene.centerCameraAt(mid_x, mid_y)
+})
+game.onUpdateInterval(2000, function () {
+    for (let value of tiles.getTilesByType(sprites.dungeon.collectibleInsignia)) {
+        if (Math.sqrt(Math.abs(value.x - player_2.x) ** 2 + Math.abs(value.y - player_2.y) ** 2) < 65) {
+            a_baddie = sprites.create(img`
+                ........................
+                ........................
+                ........................
+                ........................
+                ..........ffff..........
+                ........ff1111ff........
+                .......fb111111bf.......
+                .......f11111111f.......
+                ......fd11111111df......
+                ......fd11111111df......
+                ......fddd1111dddf......
+                ......fbdbfddfbdbf......
+                ......fcdcf11fcdcf......
+                .......fb111111bf.......
+                ......fffcdb1bdffff.....
+                ....fc111cbfbfc111cf....
+                ....f1b1b1ffff1b1b1f....
+                ....fbfbffffffbfbfbf....
+                .........ffffff.........
+                ...........fff..........
+                ........................
+                ........................
+                ........................
+                ........................
+                `, SpriteKind.Enemy)
+            tiles.placeOnTile(a_baddie, tiles.getTileLocation(value.column, value.row))
+            setBaddieVel(a_baddie)
+        }
+    }
 })
 game.onUpdateInterval(500, function () {
     monkeyMove()
